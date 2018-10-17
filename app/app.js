1ae475e0f3c1637263060f2e85d5f1e48d68879c
Aled Wassell <aledwassell@googlemail.com>
2018-10-17 09:17:09 +0100

    Update app.js

--- app/app.js
+++ app/app.js
@@ -210,8 +210,8 @@
                                 I didn't get as far as representing the real data within the graph,
                                 but I would need to convert each day's data to a to a percentage of the height of the cytoscape canvas and plot it on the y axis.
                                 */
-//                                 y: Math.random() * (cy.height() - 1) + 1//  (data.to[key][i].mid / cy.height()) × 100
-                                y: (data.to[key][i].mid / cy.height()) × 100
+                                y: Math.random() * (cy.height() - 1) + 1//  (data.to[key][i].mid / cy.height()) × 100
+                                //y: (data.to[key][i].mid / cy.height()) × 100
                             },
                             selectable: false,
                             grabbable: false
